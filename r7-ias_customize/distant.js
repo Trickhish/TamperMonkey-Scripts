cb23be961a17567ffe9f3fa735e3e7244b444afa
Charles-Ivan Dury <35201179+Trickhish@users.noreply.github.com>
2026-02-11 11:44:09 +0100

    Add UserScript for R7 IAS customization

---
+++ r7-ias_customize/distant.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         R7 IAS Customize
+// @namespace    Dury.Dev.R7customize
+// @version      2025-10-07
+// @description  Make R7 IAS better
+// @author       Charles-Ivan Dury
+// @match        https://*.appsec.insight.rapid7.com/*
+// @icon         https://t1.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://rapid7.com/&size=64
+// @grant        GM_addStyle
+// @require https://raw.githubusercontent.com/Trickhish/TamperMonkey-Scripts/refs/heads/main/r7-ias_customize/code.js
+// ==/UserScript==
